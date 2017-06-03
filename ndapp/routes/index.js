9c5341d2ca03a55e27dfc1b7c796ac0ec7303d10
Justin Haas <justinmhaas1@gmail.com>
2017-06-03 18:45:05 -0400

    added views and edited campaign.hbs

--- ndapp/routes/index.js
+++ ndapp/routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '' });
+});
+
+router.get('/campaign/', function(req, res, next) {
+  res.render('campaign', { title: 'Foo' });
+});
+
+router.get('/volunteer/', function(req, res, next) {
+  res.render('volunteer', { title: 'Foo' });
+});
+
+router.get('/organize/', function(req, res, next) {
+  res.render('organize', { title: 'Foo' });
+});
+
+router.get('/donate/', function(req, res, next) {
+  res.render('donate', { title: 'Foo' });
 });
 
 module.exports = router;
